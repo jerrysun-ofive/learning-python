9903a6220e2c30600dcd8d8b2948857164521601
jry <135770922+jerrysun-ofive@users.noreply.github.com>
2024-05-16 11:39:57 +0800

    basic fininished, need to polish
    the image works if you zoom out enough but its too large, other than that, everything works

--- img_to_ascii/img_to_ascii.py
+++ img_to_ascii/img_to_ascii.py
@@ -2,21 +2,29 @@
 from PIL import Image
 import PIL
 
-target_jpg = Image.open("5x5_img.jpg")
+# target_jpg = Image.open("5x5_img.jpg")
+target_jpg = Image.open("image_one.jpg")
+# target_jpg = Image.open("5x10_img.jpg")
+
+px = target_jpg.load()
+
 
 jpg_size = target_jpg.size
 jpg_width = jpg_size[0]
 jpg_height = jpg_size[1]
+# print(jpg_height)
+# print(jpg_width)
 pixle_array = []
 
-"`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+
+# print(px[4,9])
 
 pixle_option = ['.', '^', ':', 'i', '|', 'n', 'm', 'X', '&', '#']
 
-for i in range(jpg_width):
+for i in range(jpg_height):
     temp_array = []
-    for j in range(jpg_height):
-        this_pixle = target_jpg.getpixel((i, j))
+    for j in range(jpg_width):
+        this_pixle = px[j,i]
         brightness = (this_pixle[0] + this_pixle[1] + this_pixle[2]) / 3
 
         if brightness > 0 and brightness <= 25:
@@ -38,11 +46,21 @@ for i in range(jpg_width):
         elif brightness > 200 and brightness <= 225:
             temp_array.append(pixle_option[8])
         else:
-            pixle_array.append("!")
+            temp_array.append("#")
         #print(temp_array)
     pixle_array.append(temp_array)
 
-print(pixle_array)
+# print(pixle_array)
+
+#print(pixle_array[1039][827])
+
+for i in range(jpg_height):
+    for j in range(jpg_width):
+        print(pixle_array[i][j], end="")
+        # print(pixle_array[i][j], end="")
+    print("")
+
+
 
 
 
