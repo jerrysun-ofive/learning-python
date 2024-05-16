64887a256588391430ce2ada62b357bcf1ed27d3
jry <135770922+jerrysun-ofive@users.noreply.github.com>
2024-05-16 11:04:25 +0800

    pretty sure fixed image pixle into 2d list problem
    cant really tell if it's 100 percent correct but from what i can see it seems pretty good.
    
    might need to play with what brightness value equals to what characters though to get better results

--- img_to_ascii/img_to_ascii.py
+++ img_to_ascii/img_to_ascii.py
@@ -2,7 +2,7 @@
 from PIL import Image
 import PIL
 
-target_jpg = Image.open("/Users/jerrysun/Documents/learning-python/img_to_ascii/image_one.jpg")
+target_jpg = Image.open("5x5_img.jpg")
 
 jpg_size = target_jpg.size
 jpg_width = jpg_size[0]
@@ -14,33 +14,36 @@ pixle_array = []
 pixle_option = ['.', '^', ':', 'i', '|', 'n', 'm', 'X', '&', '#']
 
 for i in range(jpg_width):
+    temp_array = []
     for j in range(jpg_height):
         this_pixle = target_jpg.getpixel((i, j))
         brightness = (this_pixle[0] + this_pixle[1] + this_pixle[2]) / 3
 
-        #print(str(brightness) + "  ")
-
         if brightness > 0 and brightness <= 25:
-            pixle_array.append(pixle_array[0])
+            temp_array.append(pixle_option[0])
         elif brightness > 25 and brightness <= 50:
-            pixle_array.append(pixle_array[1])
+            temp_array.append(pixle_option[1])
         elif brightness > 50 and brightness <= 75:
-            pixle_array.append(pixle_array[2])
+            temp_array.append(pixle_option[2])
         elif brightness > 75 and brightness <= 100:
-            pixle_array.append(pixle_array[3])
+            temp_array.append(pixle_option[3])
         elif brightness > 100 and brightness <= 125:
-            pixle_array.append(pixle_array[4])
+            temp_array.append(pixle_option[4])
         elif brightness > 125 and brightness <= 150:
-            pixle_array.append(pixle_array[5])
+            temp_array.append(pixle_option[5])
         elif brightness > 150 and brightness <= 175:
-            pixle_array.append(pixle_array[6])
+            temp_array.append(pixle_option[6])
         elif brightness > 175 and brightness <= 200:
-            pixle_array.append(pixle_array[7])
+            temp_array.append(pixle_option[7])
         elif brightness > 200 and brightness <= 225:
-            pixle_array.append(pixle_array[8])
+            temp_array.append(pixle_option[8])
         else:
             pixle_array.append("!")
+        #print(temp_array)
+    pixle_array.append(temp_array)
 
 print(pixle_array)
+
+
 
 
