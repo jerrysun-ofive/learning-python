bfbeb7ba08e0c3761e5edb1a3e9eb0a0286b8fdc
jry <135770922+jerrysun-ofive@users.noreply.github.com>
2024-05-17 17:30:17 +0800

    random board creation
    function to randomly generate cells on the board. also changed the way the board was initialised

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -7,13 +7,37 @@
 #   5) Use python curser library to create a good UI
 ##########################################################
 
-#represent alive cells with 1 and dead cells with 0
+import random
+# represent alive cells with 1 and dead cells with 0
 
+# creates a board where every cell is dead
+# call this function at the start to get an initial, default board
 def dead_state(height: int, width: int):
-    new_board = [[0]*width]*height
+    new_board = [[0 for i in range(width)] for j in range(height)]
     return new_board
 
-return_board = dead_state(5, 10)
+# uses a rng library to determine if a cell is alive or dead to start with
+def random_state(board):
+    counter = 0
+    row_count = 0
+    item_count = 0
+    for row in board:
+        # print(f"row: {row_count}")
+        item_count = 0
+        for item in row: 
+            # print(f"item: {item_count}")
+            temp_choice = random.randrange(1, 10)
+            if temp_choice < 3:
+                board[row_count][item_count] = 1
+            item_count += 1
+            
+        row_count += 1
+    return board
 
-for row in return_board:
+# initalise a deafult board
+game_board = dead_state(5, 10)
+# creates a random 'soup' of cells to start the game
+game_board = random_state(game_board)
+
+for row in game_board:
     print(row)
