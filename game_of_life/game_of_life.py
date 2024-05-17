43fffb384b27be54af748910eb6f0258db5cad29
jry <135770922+jerrysun-ofive@users.noreply.github.com>
2024-05-17 16:42:18 +0800

    new project
    creating the classic game of life

---
+++ game_of_life/game_of_life.py
@@ -0,0 +1,8 @@
+##########################################################
+            ## milestones for this project ##
+#   1) Build a data structure to store the board state
+#   2) “Pretty-print” the board to the terminal
+#   3) Given a starting board state, calculate the next one
+#   4) Run the game forever
+#   5) Use python curser library to create a good UI
+##########################################################
